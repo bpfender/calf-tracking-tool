19d686a37c642555cbca825ed656b2fa9cde2f28
bpfender <benedict.pfender@gmail.com>
2021-06-03 20:54:47 +0100

    Trying to work out needed states for video player

--- video-tool/src/components/state/player-context.js
+++ video-tool/src/components/state/player-context.js
@@ -20,25 +20,57 @@ const defaultPlayerState = {
     networkState: null,
     paused: true,
     playbackRate: 1,
-    readyState: null,
+    readyState: 0,
     seekable: null,
     seeking: false,
     src: null,
     videoWidth: 0,
     videoHeight: 0,
 
+    // Extra states
+    currentFrame: 0,
+    seekingTime: 0,
+    seekingFrame: 0,
+    framesToSkip: 1,
+    timeToSkip: null,
+};
 
-}
+function playerReducer(state = defaultPlayerState, action) {
 
-function playerReducer(state, action) {
+    let newState = { ...state };
+
     switch (action.type) {
-        case 'play':
-        case 'pause':
-        case 'rewind':
-        case 'nextFrame':
-        case 'prevFrame':
-        case 'changeRate':
-        case 'changeFrameSkip':
+        case 'ABORT':
+        case 'CAN_PLAY':
+        case 'CAN_PLAY_THROUGH':
+        case 'DURATION_CHANGE':
+        case 'EMPTIED':
+        case 'ENDED':
+            newState.ended = true;
+            return newState;
+        case 'ERROR':
+        case 'LOADED_DATA':
+        case 'LOADED_METADATA':
+        case 'LOAD_START':
+        case 'PAUSE':
+            newState.paused = true;
+            return newState;
+        case 'PLAY':
+            newState.paused = false;
+            return newState;
+        case 'PLAYING':
+        case 'PROGRESS':
+        case 'RATE_CHANGE':
+        case 'SEEKED':
+            newState.seeking = false;
+            return newState;
+        case 'SEEKING':
+            newState.seeking = true;
+            return newState;
+        case 'STALLED':
+        case 'SUSPEND':
+        case 'TIME_UPDATE':
+        case 'WAITING':
         default: {
             throw new Error(`Unknown action type: ${action.type}`);
         }
