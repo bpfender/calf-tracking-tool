3e8c360a68e63fc4e823f983872f4602cec6253d
bpfender <benedict.pfender@gmail.com>
2021-06-04 11:25:12 +0100

    functionalised Info

--- video-tool/src/components/Info.js
+++ video-tool/src/components/Info.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-class Info extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>Media time: {this.props.videoState.currentTime}</p>
-                <p>Frame: {this.props.videoState.currentFrame} </p>
-            </div>
-        )
-    }
+
+function Info(props) {
+    return (
+        <div>
+            <p>Media time: {props.videoState.currentTime}</p>
+            <p>Frame: {props.videoState.currentFrame} </p>
+        </div>
+    )
 }
 
 export default Info;
