9bea4104d776ffed5277fa378bd33a9887935580
bpfender <benedict.pfender@gmail.com>
2021-06-04 10:45:24 +0100

    Rewriting playbackrate as functional component

--- video-tool/src/components/PlaybackRate.js
+++ video-tool/src/components/PlaybackRate.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+function PlaybackRate(props) {
+
+    const handlePlaybackRateClick = (rate) => {
+        props.video.changePlaybackRate(rate);
+    };
+
+    return (
+        <div>
+            <button onClick={() => handlePlaybackRateClick(1.0)}>1x</button>
+            <button onClick={() => handlePlaybackRateClick(2.0)}>2x</button>
+            <button onClick={() => handlePlaybackRateClick(4.0)}>4x</button>
+            <button onClick={() => handlePlaybackRateClick(8.0)}>8x</button>
+            <button onClick={() => handlePlaybackRateClick(16.0)}>16x</button>
+        </div>
+    )
+}
+
+/*
 class PlaybackRate extends React.Component {
     constructor(props) {
         super(props);
@@ -17,9 +35,11 @@ class PlaybackRate extends React.Component {
                 <button onClick={() => this.handlePlaybackRateClick(1.0)}>1x</button>
                 <button onClick={() => this.handlePlaybackRateClick(2.0)}>2x</button>
                 <button onClick={() => this.handlePlaybackRateClick(4.0)}>4x</button>
+                <button onClick={() => this.handlePlaybackRateClick(8.0)}>8x</button>
+                <button onClick={() => this.handlePlaybackRateClick(16.0)}>16x</button>
             </div>
         )
     }
 }
-
+*/
 export default PlaybackRate;
