58cb417c70e2c95f297933ff0206e6607cf92885
bpfender <benedict.pfender@gmail.com>
2021-06-04 10:59:19 +0100

    Functionalising frame navigation

--- video-tool/src/components/FrameNavigation.js
+++ video-tool/src/components/FrameNavigation.js
@@ -1,44 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-class FrameNavigation extends React.Component {
-    constructor(props) {
-        super(props)
+function FrameNavigation(props) {
 
-        this.handleRewindClick = this.handleRewindClick.bind(this);
-        this.handleForwardClick = this.handleForwardClick.bind(this);
-        this.handleBackwardClick = this.handleBackwardClick.bind(this);
-        this.handleFrameInput = this.handleFrameInput.bind(this);
-    }
+    const handleRewindClick = () => { props.video.setCurrentTime(0) };
+    const handleForwardClick = () => { props.video.nextFrame() };
+    const handleBackwardClick = () => { props.video.prevFrame() };
+    const handleFrameInput = (event) => props.video.setCurrentFrame(event.target.value);
 
-    handleRewindClick() {
-        this.props.video.setCurrentTime(0.0);
-    }
 
-    handleForwardClick() {
-        console.log("Frame forward click");
-        this.props.video.nextFrame();
-    }
-
-    handleBackwardClick() {
-        console.log("Frame backward click");
-        this.props.video.prevFrame();
-    }
-
-    handleFrameInput(event) {
-        this.props.video.setCurrentFrame(event.target.value);
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.handleRewindClick}> REWIND</button>
-                <button onClick={this.handleForwardClick}>FRAME FORWARD</button>
-                <button onClick={this.handleBackwardClick}>FRAME BACKWARD</button>
-                <input onChange={this.handleFrameInput}></input>
-            </div>
-        )
-    }
-
+    return (
+        <div>
+            <button onClick={handleRewindClick}> REWIND</button>
+            <button onClick={handleForwardClick}>FRAME FORWARD</button>
+            <button onClick={handleBackwardClick}>FRAME BACKWARD</button>
+            <input onChange={handleFrameInput}></input>
+        </div>
+    )
 }
 
 export default FrameNavigation;
