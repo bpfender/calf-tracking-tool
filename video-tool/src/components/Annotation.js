a60cfceb9a52f2f9e6614c391f0efcaf283a7acb
bpfender <benedict.pfender@gmail.com>
2021-06-09 14:04:27 +0100

    Added annotation canvas

--- video-tool/src/components/Annotation.js
+++ video-tool/src/components/Annotation.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Annotation(props) {
     return (
-        <canvas id="annotation-canvas"></canvas>
+        <canvas
+            id="annotation-canvas"
+            width="800px"
+            height="600px">
+        </canvas>
     );
 }
 
