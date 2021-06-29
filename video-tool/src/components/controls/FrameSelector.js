21bdcba7c243686b89cb4704c8918a4556eac01d
bpfender <benedict.pfender@gmail.com>
2021-06-29 16:31:27 +0100

    Working on styling for frameselector

---
+++ video-tool/src/components/controls/FrameSelector.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { InputGroup } from "@blueprintjs/core";
+import "./FrameSelector.scss";
+
+
+export default function FrameSelector(props) {
+    const { video, playerState } = props;
+    const [currentFrame, setCurrentFrame] = useState(1);
+    const [intent, setIntent] = useState("none");
+
+    // TODO update total frames based on player state
+    const frameCount = 86302
+
+    useEffect(() => {
+        setCurrentFrame(playerState.currentFrame);
+        setIntent("none");
+    }, [playerState.currentFrame]);
+
+    useEffect(() => {
+        if (playerState.seeking) {
+            setIntent("warning");
+        } else {
+            setIntent("success")
+        }
+    }, [playerState.seeking])
+
+    // FIXME not sure this timeout is working properly
+    // FIXME limit numerical input and validate input
+    const changeHandler = (event) => {
+        setTimeout(video.setCurrentFrame, 1000, parseInt(event.target.value));
+    }
+
+    // TODO possible to select all on click?
+    return (
+        <div className="frame-selector">
+            <InputGroup
+                onChange={changeHandler}
+                type="number"
+                leftIcon="duplicate"
+                value={currentFrame}
+                placeholder={1}
+                min={1}
+                max={frameCount}
+                step={1}
+                asyncControl={true}
+                intent={intent}
+                small={true}
+            ></InputGroup>
+            <span className="frame-count">/ {frameCount}</span>
+        </div >
+    );
+}
