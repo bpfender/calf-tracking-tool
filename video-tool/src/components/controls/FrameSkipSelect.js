85414e2efb96515aae098566f078919e8ad74d3c
bpfender <benedict.pfender@gmail.com>
2021-07-10 12:19:49 +0100

    Basic interpolation functionality working. Code needs cleaning up

--- video-tool/src/components/controls/FrameSkipSelect.js
+++ video-tool/src/components/controls/FrameSkipSelect.js
@@ -18,6 +18,7 @@ export default function FrameSkipSelect(props) {
             <option value="20">20</option>
             <option value="50">50</option>
             <option value="100">100</option>
+            <option value="500">500</option>
         </HTMLSelect >
     );
 }
