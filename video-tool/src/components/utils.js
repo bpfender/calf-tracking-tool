c447422bdcaca8639d446839cd73fa4340cd8a7b
bpfender <benedict.pfender@gmail.com>
2021-07-10 15:29:10 +0100

    Added new frame fps calc function. Much improved

--- video-tool/src/components/utils.js
+++ video-tool/src/components/utils.js
@@ -1,22 +1,31 @@
 // TODO this is very rudimentary currently. Will need to be integrated into setup routine
-function calculateFPS(now) {
-    if (typeof this.calculateFPS.start_time == 'undefined') {
-        this.calculateFPS.start_time = 0.0;
-        this.calculateFPS.count = 0;
+export function calculateFPS(now) {
+    if (typeof calculateFPS.start_time === 'undefined') {
+        calculateFPS.start_time = 0.0;
+        calculateFPS.count = 0;
     }
 
-    if (this.calculateFPS.start_time === 0.0) {
-        this.calculateFPS.start_time = now;
+    if (calculateFPS.start_time === 0.0) {
+        calculateFPS.start_time = now;
     }
 
-    let elapsed = (now - this.calculateFPS.start_time) / 1000.0;
-    let fps = ++this.calculateFPS.count / elapsed;
+    let elapsed = (now - calculateFPS.start_time) / 1000.0;
+    let fps = ++calculateFPS.count / elapsed;
 
-    if (this.calculateFPS.count > 40) {
-        this.calculateFPS.start_time = 0.0;
-        this.calculateFPS.count = 0;
-        console.log(Math.floor(fps));
+    if (calculateFPS.count > 40) {
+        calculateFPS.start_time = 0.0;
+        calculateFPS.count = 0;
+        console.log((fps));
     }
 };
 
-export { calculateFPS };
+export function calcFPS2(metadata) {
+    if (typeof calcFPS2.count === 'undefined' ||
+        calcFPS2.count > 10) {
+        calcFPS2.count = 0;
+        calcFPS2.time = metadata.mediaTime;
+    }
+
+    const fps = calcFPS2.count++ / (metadata.mediaTime - calcFPS2.time);
+    console.log(fps);
+}
