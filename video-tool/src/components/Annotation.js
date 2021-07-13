b4452de1da851a31c84c9d753da3f5d9b897d9cb
bpfender <>
2021-07-13 18:03:32 +0100

    Some consolidation

--- video-tool/src/components/Annotation.js
+++ video-tool/src/components/Annotation.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { getBoundingBoxes } from './annotations/Annotations';
 import Scene from './canvas/Scene';
 
-
 // FIXME canvas resizing and scaling
 function Annotation(props) {
     const { annotations, annotationDispatch, currentFrame } = props;
