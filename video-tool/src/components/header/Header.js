587e8d82fbdd4c8d39f5deb133ccd8cc938a1714
bpfender <>
2021-08-02 17:14:52 +0100

    Added some function signatures for Header

--- video-tool/src/components/header/Header.js
+++ video-tool/src/components/header/Header.js
@@ -4,33 +4,40 @@ import SourceSelector from '../SourceSelector';
 
 export function Header(props) {
     const { framerate, playerDispatch, annotations } = props;
-
     const [openFile, setOpenFile] = useState("folder-close");
+
+    const handleNewProject = () => { };
+    const handleSave = () => { };
+    const handleOpen = () => { };
+
+
 
     return (
         <header className={props.className}>
-
             <ButtonGroup
                 minimal={true}>
-                <Button icon="document" />
+                <Button
+                    icon="document"
+                    onClick={handleNewProject} />
                 <Button
                     icon={openFile}
+                    onClick={handleOpen}
                     onMouseEnter={() => { setOpenFile("folder-open") }}
                     onMouseLeave={() => { setOpenFile("folder-close") }} />
-                <Button icon="floppy-disk" />
-                <Button icon="export" />
+                <Button
+                    icon="floppy-disk"
+                    onClick={handleSave} />
+                <Button
+                    icon="export" />
             </ButtonGroup>
+            <title>PROJECT NAME</title>
             <Icon icon="help"></Icon>
-
-
 
             <SourceSelector
                 fps={framerate}
                 playerDispatch={playerDispatch}
                 annotations={annotations}>
             </SourceSelector>
-
         </header>
-
     );
 }
