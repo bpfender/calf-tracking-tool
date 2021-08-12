c3da2f8443c6f9149ea536a593bfc5d289f5805b
bpfender <>
2021-08-12 16:46:41 +0100

    Renamed the right file

---
+++ video-tool/src/components/storage/file-access.js
@@ -0,0 +1,96 @@
+//https://web.dev/file-system-access/
+export async function getParentDirectory() {
+    const dirHandle = await window.showDirectoryPicker({ startIn: 'documents' });
+    return dirHandle;
+}
+
+export async function createNewProjectHandle(dirHandle, filename) {
+    const handle = await dirHandle.getFileHandle(filename + ".vat", { create: true });
+    return handle;
+}
+
+export async function getProjectHandle(dirHandle) {
+    const startIn = dirHandle ? dirHandle : 'documents';
+    const options = {
+        startIn: startIn,
+        types: [
+            {
+                description: 'VAT Project File',
+                accept: { 'application/json': ['.vat'] }
+            },
+        ],
+        multiple: false,
+    };
+
+    const [handle] = await window.showOpenFilePicker(options);
+    return handle;
+}
+
+//TODO check MIME types
+export async function getVideoHandle(dirHandle) {
+    const startIn = dirHandle ? dirHandle : 'video';
+    const options = {
+        startIn: startIn,
+        types: [
+            {
+                description: 'Video file',
+                accept: {
+                    'video/mp4': ['.mp4'],
+                    'video/mpeg': ['.mpeg'],
+                    'video/ogg': ['.ogv'],
+                    'video/webm': ['.webm'],
+                },
+                multiple: false,
+            }
+        ]
+    }
+
+    const [handle] = await window.showOpenFilePicker(options);
+    return handle;
+}
+
+
+export async function readFile() {
+
+}
+
+export async function writeFile(fileHandle, data) {
+    const writable = await fileHandle.createWritable()
+    await writable.write(data);
+    await writable.close();
+}
+
+export async function verifyPermission(handle) {
+    const options = {
+        mode: 'readwrite',
+    };
+
+    if (await handle.queryPermission(options) === 'granted') {
+        return true;
+    }
+
+    if (await handle.requestPermission(options) === 'granted') {
+        return true;
+    }
+
+    return false;
+}
+
+export async function getVideoDirHandle(parentDir) {
+    const videoDirHandle = await parentDir.getDirectoryHandle("videos", { create: true });
+}
+
+
+export async function selectMoveVideoIntoProject(dirHandle) {
+    const videoSrcHandle = await getVideoHandle(dirHandle);
+    const videoDirHandle = await getVideoDirHandle(dirHandle);
+    const videoDestHandle = await videoDirHandle.getFileHandle(videoSrcHandle.name, { create: true })
+
+    const videoSrcFile = await videoSrcHandle.getFile();
+    const videoSrcStream = await videoSrcFile.stream();
+    const videoDestStream = await videoDestHandle.createWritable();
+
+    await videoSrcStream.pipeTo(videoDestStream);
+
+    return videoDestHandle;
+}
