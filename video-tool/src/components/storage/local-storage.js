670d169f3a4cc28a40a50daf85b32884c954f7ac
bpfender <>
2021-08-12 16:38:42 +0100

    Seperating out indexeddb functions

--- video-tool/src/components/storage/local-storage.js
+++ video-tool/src/components/storage/local-storage.js
@@ -1,22 +1,30 @@
 import { get, set } from "idb-keyval";
 
-export function storeCurrentVideoFile(file) {
-    set("file", file)
-        .then(() => console.log("FILE SAVED"))
-        .catch((err) => console.log("FAILURE", err));
+const recent = 'recent';
+const appDir = 'appDir';
+const autoSave = 'autoSave';
+
+
+export async function setAppDirectoryHandle(dirHandle) {
+    await set(appDir, dirHandle);
 }
 
-export async function getLastVideoFile() {
-    return await get("file").then((val) => val);
+export async function getAppDirectoryHandle() {
+    await get(appDir);
 }
 
-//FIXME error handling
-export function storeAnnotationData(annotations) {
-    set("annotations", annotations)
-        .then(() => console.log("ANNOTATIONS SAVED"))
-        .catch((err) => console.log("FAILURE", err));
+export async function setAutoSave(saveData) {
+    await set(autoSave, saveData);
 }
 
-export async function getAnnotationData() {
-    return await get("annotations");
+export async function getAutoSave() {
+    await get(autoSave);
+}
+
+export async function setRecentProjectHandle(fileHandle) {
+    await set(recent, fileHandle);
+}
+
+export async function getRecentProjectHandle() {
+    return await get(recent);
 }
